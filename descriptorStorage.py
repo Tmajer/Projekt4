9d95b8bc4dd83aa6cfd08642c90f7042dfa0f6c0
Marek Hruz <mhruz@kky.zcu.cz>
2020-08-27 11:31:49 +0200

    MHR - TODOs

--- descriptorStorage.py
+++ descriptorStorage.py
@@ -5,7 +5,8 @@ import cv2 as cv
 import glob
 
 
-def init_db(filename: str, path_to_images: str, matcher : FeatureMatcher, mapping_file : str = 'data.txt', des_dataset: str = 'DatasetDescriptors', kp_dataset: str = 'DatasetKeypoints'):
+def init_db(filename: str, path_to_images: str, matcher: FeatureMatcher, mapping_file: str = 'data.txt',
+            des_dataset: str = 'DatasetDescriptors', kp_dataset: str = 'DatasetKeypoints'):
     """Initializes h5py file or overwrites the existing one, resets image mapping
 
         Creates new h5py file with given name via filename parameter. This file will always have two datasets, descriptor dataset
@@ -53,7 +54,8 @@ def init_db(filename: str, path_to_images: str, matcher : FeatureMatcher, mappin
                 des_count, _ = des.shape
                 keypoints = []
                 for point in kp:
-                    temp = (point.pt[0], point.pt[1], point.size, point.angle, point.response, point.octave, point.class_id)
+                    temp = (
+                        point.pt[0], point.pt[1], point.size, point.angle, point.response, point.octave, point.class_id)
                     keypoints.append(temp)
                 end_index = start_index + des_count
                 des_set[start_index:end_index, ...] = des
@@ -67,7 +69,8 @@ def init_db(filename: str, path_to_images: str, matcher : FeatureMatcher, mappin
             json.dump(mapping, out)
 
 
-def update_db(filename: str, path_to_images: str, matcher : FeatureMatcher, mapping_file : str = 'data.txt', des_dataset: str = 'DatasetDescriptors', kp_dataset: str = 'DatasetKeypoints'):
+def update_db(filename: str, path_to_images: str, matcher: FeatureMatcher, mapping_file: str = 'data.txt',
+              des_dataset: str = 'DatasetDescriptors', kp_dataset: str = 'DatasetKeypoints'):
     """Updates existing h5py file with new image descriptors and keypoints
 
         Updates existing h5py file to contain new images added to path_to_images directory. This file will always have two datasets, descriptor dataset
@@ -93,12 +96,14 @@ def update_db(filename: str, path_to_images: str, matcher : FeatureMatcher, mapp
 
     with h5py.File(filename, 'a') as f:
         if not des_dataset in f.keys():
+            # TODO: estimate number of kp beforehand
             des_set = f.create_dataset(des_dataset, (500000, 128), maxshape=(None, 128))
         des_set = f[des_dataset]
         if not kp_dataset in f.keys():
             kp_set = f.create_dataset(kp_dataset, (500000, 7), maxshape=(None, 7))
         kp_set = f[kp_dataset]
 
+        # TODO: make mapping as a h5 datasets (filenames, intervals)
         with open(mapping_file) as jfile:
             mapping = json.load(jfile)
 
@@ -108,15 +113,19 @@ def update_db(filename: str, path_to_images: str, matcher : FeatureMatcher, mapp
         start_index = mapping['start_index_for_new']
 
         for image in imgs:
+            #des_set.create_group(image, (100, 128), maxshape=(None, 128))
             img = cv.imread(image, cv.IMREAD_GRAYSCALE)
             if (not image in mapping) or not mapping[image]:
+                # TODO: create dataset after knowing des and kp dimensionality
                 kp, des = matcher.detect(img)
                 des_count, _ = des.shape
                 keypoints = []
                 for point in kp:
-                    temp = (point.pt[0], point.pt[1], point.size, point.angle, point.response, point.octave, point.class_id)
+                    temp = (
+                        point.pt[0], point.pt[1], point.size, point.angle, point.response, point.octave, point.class_id)
                     keypoints.append(temp)
                 end_index = start_index + des_count
+                # TODO: chcek if there is space in db, otherwise resize
                 des_set[start_index:end_index, ...] = des
                 kp_set[start_index:end_index, ...] = keypoints
 
@@ -128,7 +137,8 @@ def update_db(filename: str, path_to_images: str, matcher : FeatureMatcher, mapp
             json.dump(mapping, out)
 
 
-def get_des(filename: 'str', image_path: 'str', mapping_file : str = 'data.txt', des_dataset: str = 'DatasetDescriptors'):
+def get_des(filename: 'str', image_path: 'str', mapping_file: str = 'data.txt',
+            des_dataset: str = 'DatasetDescriptors'):
     """Looks into existing h5py file and returns descriptors for given image
 
         This method looks into h5py file given by filename parameter and looks for descriptors in dataset des_dataset.
@@ -154,7 +164,7 @@ def get_des(filename: 'str', image_path: 'str', mapping_file : str = 'data.txt',
         return descriptor
 
 
-def get_kp(filename: 'str', image_path: 'str', mapping_file : str = 'data.txt', kp_dataset: str = 'DatasetKeypoints'):
+def get_kp(filename: 'str', image_path: 'str', mapping_file: str = 'data.txt', kp_dataset: str = 'DatasetKeypoints'):
     """Looks into existing h5py file and returns keypoints for given image
 
         This method looks into h5py file given by filename parameter and looks for keypoints in dataset kp_dataset.
@@ -179,33 +189,35 @@ def get_kp(filename: 'str', image_path: 'str', mapping_file : str = 'data.txt', 
         kp = []
 
         for i in range(mapping[image_path][0], mapping[image_path][1]):
-            temp = cv.KeyPoint(x = kpset[i][0], y=kpset[i][1], _size=kpset[i][2], _angle=kpset[i][3],  _response=kpset[i][4], _octave=kpset[i][5], _class_id=kpset[i][6])
+            temp = cv.KeyPoint(x=kpset[i][0], y=kpset[i][1], _size=kpset[i][2], _angle=kpset[i][3],
+                               _response=kpset[i][4], _octave=kpset[i][5], _class_id=kpset[i][6])
             kp.append(temp)
 
         return kp
 
 
-def reset_db(mapping_file : str):
+def reset_db(mapping_file: str):
     """This method empties the mapping file"""
 
     with open(mapping_file, 'w') as out:
-            mapping = {}
-            json.dump(mapping, out)
+        mapping = {}
+        json.dump(mapping, out)
 
 
 if __name__ == '__main__':
-    image1 = './img\\bb_02.jpg'
+    image1 = './img/bb_02.jpg'
     img1 = cv.imread('bb_02.jpg', cv.IMREAD_GRAYSCALE)  # Reference Image
     imgs = glob.glob('./img/*.jpg')
 
-    sift = cv.xfeatures2d.SIFT_create()
+    # sift = cv.xfeatures2d.SIFT_create()
+    sift = cv.SIFT_create()
     FLANN_INDEX_KDTREE = 0
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
     search_params = dict(checks=50)
     flann = cv.FlannBasedMatcher(index_params, search_params)
     siftFlann = FeatureMatcher(sift, flann)
     # init_db('siftFile2.hdf5', './img/*.jpg', siftFlann) # FOR FIRST RUN ONLY
-    update_db('siftFile2.hdf5', './img/*.jpg', siftFlann) # EVERY OTHER RUN
+    update_db('siftFile2.hdf5', './img/*.jpg', siftFlann)  # EVERY OTHER RUN
 
     with h5py.File('siftFile.hdf5', 'w') as f:
         if not 'siftDatasetDescriptors' in f.keys():
@@ -226,7 +238,8 @@ if __name__ == '__main__':
                 des_count, _ = des.shape
                 keypoints = []
                 for point in kp:
-                    temp = (point.pt[0], point.pt[1], point.size, point.angle, point.response, point.octave, point.class_id)
+                    temp = (
+                        point.pt[0], point.pt[1], point.size, point.angle, point.response, point.octave, point.class_id)
                     keypoints.append(temp)
                 end_index_des = start_index_des + des_count
                 des_set[start_index_des:end_index_des, ...] = des
@@ -236,13 +249,13 @@ if __name__ == '__main__':
                 start_index_des += des_count
                 mapping['start_index_for_new'] = start_index_des
     with open('test.txt', 'w') as out:
-            json.dump(mapping, out)
+        json.dump(mapping, out)
 
     for image in imgs:
         des_method = get_des('siftFile2.hdf5', image)
-        des_nomethod = get_des('siftFile.hdf5', image, mapping_file='test.txt', des_dataset= 'siftDatasetDescriptors')
+        des_nomethod = get_des('siftFile.hdf5', image, mapping_file='test.txt', des_dataset='siftDatasetDescriptors')
         kp_method = get_kp('siftFile2.hdf5', image)
-        kp_nomethod = get_kp('siftFile.hdf5', image, mapping_file='test.txt', kp_dataset= 'siftDatasetKeypoints')
+        kp_nomethod = get_kp('siftFile.hdf5', image, mapping_file='test.txt', kp_dataset='siftDatasetKeypoints')
 
         print('Some descriptors')
         print(des_method[1][0:5])
@@ -250,9 +263,6 @@ if __name__ == '__main__':
 
         print('Some keypoints')
         print(kp_method[0].pt, kp_method[0].size, kp_method[0].angle)
-        print(kp_nomethod[0].pt,kp_nomethod[0].size, kp_nomethod[0].angle)
+        print(kp_nomethod[0].pt, kp_nomethod[0].size, kp_nomethod[0].angle)
 
     reset_db('test.txt')
-
-
-
